0ad3f1f647b7bc0613ad4422ef1068956bcd35cc
Lipevic <felipe.9000@hotmail.com>
2023-07-05 14:49:13 -0300

    base config

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className=" text-4xl font-bold">
+        Sua pagina predileta de videos e fotos!
+      </h1>
+    </div>
+  )
+}
